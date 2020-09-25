0186c7daff5eeabcd14e13b543d3a21b08edc05b
bigbother <793041663@qq.com>
2020-09-26 00:03:54 +0800

    uid

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -55,7 +55,7 @@ function Payment() {
         //paymentIntent = payment confirmation
 
         db.collection("users")
-          .doc(user?.id)
+          .doc(user?.uid)
           .collection("orders")
           .doc(paymentIntent.id)
           .set({
