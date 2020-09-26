06265ad2108a704360795dbdbdf2f6387c267fd7
bigbother <793041663@qq.com>
2020-09-26 20:54:39 +0800

    finish without detail

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const { functions } = require("firebase");
+const { static } = require("express");
 const stripe = require("stripe")(
   "sk_test_51HV7CvHOkPbYveuVIYgmkbVrUOyo7CfdPsvtknxF3nWnmKEspsO8UXYSzHbQq0AjruV3zGlfLrNwxjq0VhTeSjW800y4k4NBXJ"
 );
@@ -18,9 +20,12 @@ app.use(
     extended: false,
   })
 );
+app.use(express.static(path.join(__dirname, "build")));
 
 //-API routes
-app.get("/", (req, res) => res.status(200).send("Hello world"));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "build", "index.html"))
+);
 
 app.post("/api/payments/create", async (req, res) => {
   const total = Math.round(req.query.total);
